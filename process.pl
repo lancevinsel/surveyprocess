e271cf669b44a94cc0804dedbbd2dfd9419a9a06
Lance Vinsel <lvinsel@mps-il.com>
2017-06-28 11:04:11 -0500

    added flag/switch for Civil3d or Openroads in the process.pl

--- process.pl
+++ process.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 use lib 'C:\git-repos\surveyprocess\modules';
+use Getopt::Long qw(getOptions);
 
 use commentLists;
 use flagCodes;
@@ -9,22 +10,26 @@ use mainCodes;
 use liningCodes;
 use noLineList;
 # Some Vars
+my @in="";
 my $fullCode="";
 my $fullComment="";
 my $lineCode="";
 my $liningSymbol="";
 my $IDOTlineSymbol="";
 my $ACADlineSymbol="";
-$figname="";
-$curPtNum="";
-$lastPtNum="";
-$lastFigname="";
-%activeStrings=();
-$lastWasString=0;
-$comment=""; #lv added
-$Icode=""; #lv added (Process01)
-$hold = "";
-$noLineCounter=1;
+my $filename="";
+my $civil3d="";
+my $openroads="";
+# $figname="";
+# $curPtNum="";
+# $lastPtNum="";
+# $lastFigname="";
+# %activeStrings=();
+# $lastWasString=0;
+# $comment=""; #lv added
+# $Icode=""; #lv added (Process01)
+# $hold = "";
+# $noLineCounter=1;
 
 
 # ==============================================================================================
@@ -39,7 +44,26 @@ if ($#ARGV<0) {
 |                                                                            |
 |                     Process script requires file name                      |
 |                                                                            |
-|                        Syntax:  process filename                           |
+|                    Syntax:  process filename -c (or -i)                    |
+|                                                                            |
+==============================================================================
+
+};
+	exit;
+}
+# Test for the output program flag and assign a value to either $civil3d or $openroads
+GetOptions('c' => \$civil3d);
+GetOptions('o' => \$openroads);
+if ($civil3d = "" && $openroads = ""){
+	print q{
+==============================================================================
+|                                                                            |
+|             Hey - The process script now requires a -c or a -o             |
+|              The -c is for Civil3d and the -o is for Openroad              |
+|                                                                            |
+|                        Syntax:  process filename -c                        |
+|                                     or                                     |
+|                        Syntax:  process filename -o                        |
 |                                                                            |
 ==============================================================================
 
@@ -57,6 +81,10 @@ open(IN,$ARGV[0]);
 # This opens the file for writing
 open(OUT,">${filename}_all.cor");
 # begin the loop - read in the lines one by one and evaluate to the end
+# eliminate specific warnings - this is needed to prevent error messages in this code
+{
+	no warnings 'uninitialized';
+	no warnings 'once';
 while (<IN>) {
 	@in = split(/,/, substr(uc, 0, -1), 5); # the substr forces text to be uppercase
 	# the split creates:
@@ -89,7 +117,7 @@ while (<IN>) {
 		my $combinedLineSymbol = ($liningCodes::lineSymbols{$liningSymbol});
 		 print OUT "combinedLineSymbol		= $combinedLineSymbol\n";
 		my @separateLineSymbol = split(/\s+/,$combinedLineSymbol,2); # this separates
-		# the IDOTlineSymbol from the ACADlineSymbol 
+		# the IDOTlineSymbol from the ACADlineSymbol
 		 print OUT "separateLineSymbol[0]	= $separateLineSymbol[0]\n";
 		 print OUT "separateLineSymbol[1]	= $separateLineSymbol[1]\n";
 		$IDOTlineSymbol = $separateLineSymbol[0];
@@ -117,31 +145,31 @@ while (<IN>) {
 	$lineNumber = $lineNumberSplit[1];
 		# print OUT "lineNumber		= $lineNumber\n";
 
-
-
-		######33333333333333333333
-
- my @fsplit = split(/\s+/,$in[4],2); #added lv - this separates the Codes from the Comments
-   #using the first whitespace as the separator so:
-   #$fsplit[0] = 3 Letter Code-Line Number-Line Code
-   #$fsplit[1] = the Comment
- my @ssplit = ($fsplit[0] =~ /(\w+)*(\W*)/); #added lv - this separtes the 3 Letter Code and
-   # line number from the line coding symbol
-   # \w is alpha or numeric - \W is non alpha or numeric:
-   # $ssplit[0] = the 3 Letter Code and Line Number
-   # $ssplit[1] = the Line Code
- $tok[0] = $ssplit[1]; #added lv:
-   # $tok[0} = the Line Code
- $tok[1] = "$ssplit[0] $fsplit[1]"; #added lv:
-   # $tok[1] = the code and the comment, no line code
- my @csplit = ($ssplit[0] =~ /(\w\w\w)(\d*)/); # this is for QAQC
-   # $csplit[0] = 3 letter code
-   # $csplit[1] = line number
-# if (length($fsplit[1])>0) {##############lv
-#  $fsplit[1]="\;$fsplit[1]"; ########lv - adds the semi-colon before the Comment
-# }
- $Icode=$pointCodes{$csplit[0]}; ## if the three letter code matches any of the codes
-
+#
+#
+# 		######33333333333333333333
+#
+#  my @fsplit = split(/\s+/,$in[4],2); #added lv - this separates the Codes from the Comments
+#    #using the first whitespace as the separator so:
+#    #$fsplit[0] = 3 Letter Code-Line Number-Line Code
+#    #$fsplit[1] = the Comment
+#  my @ssplit = ($fsplit[0] =~ /(\w+)*(\W*)/); #added lv - this separtes the 3 Letter Code and
+#    # line number from the line coding symbol
+#    # \w is alpha or numeric - \W is non alpha or numeric:
+#    # $ssplit[0] = the 3 Letter Code and Line Number
+#    # $ssplit[1] = the Line Code
+#  $tok[0] = $ssplit[1]; #added lv:
+#    # $tok[0} = the Line Code
+#  $tok[1] = "$ssplit[0] $fsplit[1]"; #added lv:
+#    # $tok[1] = the code and the comment, no line code
+#  my @csplit = ($ssplit[0] =~ /(\w\w\w)(\d*)/); # this is for QAQC
+#    # $csplit[0] = 3 letter code
+#    # $csplit[1] = line number
+# # if (length($fsplit[1])>0) {##############lv
+# #  $fsplit[1]="\;$fsplit[1]"; ########lv - adds the semi-colon before the Comment
+# # }
+#  $Icode=$pointCodes{$csplit[0]}; ## if the three letter code matches any of the codes
+#
 ##############Test Section
 ##print OUT1 "alksdf;alkdj   $pointCodes{$csplit[0]}\n";
 #print OUT1 "\n\n\n\in[0] point number      = $in[0]\n";
@@ -157,11 +185,11 @@ while (<IN>) {
 ##print OUT1 "hold                           = $hold\n";
 ##print OUT1 "c linecode                     = $c\n";
 #print OUT1 "Icode idot code, line no.      = $Icode\n\n";
-        #####################
-        #Material type prefix
-        if  (exists ($typePrefix{$csplit[0]})) {
-          $prefix = $typePrefix{$csplit[0]};
-          $csplit[1] = "$prefix$csplit[1]";
+#####################
+#Material type prefix
+	if  (exists ($typePrefixList::typePrefix{$MPScode})) {
+  $prefix = $typePrefixList::typePrefix{$MPScode};
+  $lineNumber = "$prefix$lineNumber";
  }
 # ########################################
 # #NoLine fix
@@ -252,7 +280,7 @@ while (<IN>) {
  $csplit[0]="";
  $csplit[1]="";
 
-
+}
 }
 close(IN);
 close(OUT1);
