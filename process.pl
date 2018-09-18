1503fde0a9eb6b12b30213914ebe8618211043bc
Lance Vinsel <lvinsel@mps-il.com>
2018-09-18 10:35:50 -0500

    bend section

--- process.pl
+++ process.pl
@@ -1,14 +1,4 @@
 #!/bin/perl
-%pointCodes = (  # ABC -> 123
- #"XXX" => "XXX",
- "ACU" => "282", # Air Conditioning Unit
- "AEM" => "930", # AG Electric Main
- #"AGE" => "674", # Shoulder Aggregate Edge
- "B62" => "516", # Curb B6-24 Top Back - depreciated
- "B6B" => "509", # Curb B6-12 Top Back - depreciated
- "B6F" => "510" # Curb B6-12 Flowline #deleted v6 # added back v15 - depreciated
- );
-
 #-------------------------------
 # Some Global Vars
 $lastweld="";
@@ -51,39 +41,33 @@ while (<IN>) {
     if ($code eq "WLD") {
         $lastbend="";
         if ($lastjoint) {
-            $jointstation = (($station+$lastweld)/2)
+            $jointstation = (($station+$lastweld)/2);
             print OUT1 "JNT,$jointstation,$jointtext1,$jointtext2\n";
         }
         if ($lastdist) {
-            $diststation = (($station+$lastweld)/2)
+            $diststation = (($station+$lastweld)/2);
             print OUT1 "DST,$diststation,$disttext1,\n";
        }
        print OUT1 "WLD,$station,$text1,\n";
+       $lastweld=$station;
+    }
+    if ($code eq "BND") {
+        $bendtext1=$text1
+        if ($lastbend) {
+            $legstation = (($station+$lastbend)/2);
+            print OUT1 "LEG,$legstation,,\n";
+            print OUT1 "BND,$station,$bendtext1,\n;
+            $lastbend=$station;
+        } else {
+            $legstation = (($station+$lastweld)/2);
+            print OUT1 "LEG,$legstation,,\n";
+            print OUT1 "BND,$station,$bendtext1,\n;
+            $lastbend=$station;
+        }
     }
 
 
 
-
- my @fsplit = split(/\s+/,$in[4],2); #added lv - this separates the Codes from the Comments
-   #using the first whitespace as the separator so:
-   #$fsplit[0] = 3 Letter Code-Line Number-Line Code
-   #$fsplit[1] = the Comment
- my @ssplit = ($fsplit[0] =~ /(\w+)*(\W*)/); #added lv - this separtes the 3 Letter Code and
-   # line number from the line coding symbol
-   # \w is alpha or numeric - \W is non alpha or numeric:
-   # $ssplit[0] = the 3 Letter Code and Line Number
-   # $ssplit[1] = the Line Code
- $tok[0] = $ssplit[1]; #added lv:
-   # $tok[0} = the Line Code
- $tok[1] = "$ssplit[0] $fsplit[1]"; #added lv:
-   # $tok[1] = the code and the comment, no line code
- my @csplit = ($ssplit[0] =~ /(\w\w\w)(\d*)/); # this is for QAQC
-   # $csplit[0] = 3 letter code
-   # $csplit[1] = line number
- if (length($fsplit[1])>0) {##############lv
-  $fsplit[1]="\;$fsplit[1]"; ########lv - adds the semi-colon before the Comment
- }
- $Icode=$pointCodes{$csplit[0]}; ## if the three letter code matches any of the codes
 
 ##############Test Section
 #print OUT1 "alksdf;alkdj   $pointCodes{$csplit[0]}\n";
@@ -100,20 +84,6 @@ while (<IN>) {
 #print OUT5 "hold                           = $hold\n";
 #print OUT5 "c linecode                     = $c\n";
 #print OUT5 "Icode idot code, line no.      = $Icode\n\n";
-        #####################
-        #Material type prefix
-        if  (exists ($typePrefix{$csplit[0]})) {
-          $prefix = $typePrefix{$csplit[0]};
-          $csplit[1] = "$prefix$csplit[1]";
- }
- ########################################
- #NoLine fix
- if  (exists ($noLine{$csplit[0]})) {
-          $csplit[1] = $noLineCounter;
-          $noLineCounter = $noLineCounter + 1;
- }
- ########################################Begin sorting and printing
- #################################################
  if  (exists ($bridgeCodes{$csplit[0]}))
    {
    if ($c = $idotcommands{$tok[0]})
