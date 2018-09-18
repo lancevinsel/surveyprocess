b19de12d54e4f9a259ced16fdc8833276c93e2c6
Lance Vinsel <lvinsel@mps-il.com>
2018-09-18 16:39:54 -0500

    weld process done - I think!

--- process.pl
+++ process.pl
@@ -1,15 +1,18 @@
 #!/bin/perl
 #-------------------------------
 # Some Global Vars
-$lastweld="";
-$lastjoint="";
-$lasttext1="";
-$lastdist="";
-$lastbend="";
+$bendtext1="";
+$diststation="";
+$disttext1="";
+$jointstation="";
 $jointtext1="";
 $jointtext2="";
-$disttext1="";
-$bendtext1="";
+$lastbend="";
+$lastdist="";
+$lastjoint="";
+$lasttext1="";
+$lastweld="";
+$legstation="";
 
 # ====================================================================================================
 #                                             Start of Main Program
@@ -38,79 +41,75 @@ while (<IN>) {
     $station = $in[1];
     $text1   = $in[2];
     $text2   = $in[3];
+#                    print OUT1 "READ code is                             $code\n";
+#                    print OUT1 "READ station is                          $station\n";
+#                    print OUT1 "READ text1 is                            $text1\n";
+#                    print OUT1 "READ text2 is                            $text2\n";
+
+    if ($code eq "JNT") {
+        $jointtext1=$text1;
+#                    print OUT1 "IF JNT jointtext1                         $jointtext1\n";
+        $jointtext2=$text2;
+#                    print OUT1 "IF JNT jointtext2                         $jointtext2\n";
+    }
+    if ($code eq "DST") {
+        $disttext1=$text1;
+#                    print OUT1 "IF DST disttext1                         $disttext1\n";
+    }
     if ($code eq "WLD") {
+	if ($lastbend) {
+            $legstation = (($station+$lastbend)/2);
+                    print OUT1 "LEG,$legstation,,\n";
+	}
         $lastbend="";
+#                    print OUT1 "IF WLD lastbend                         $lastbend\n";
+#                    print OUT1 "IF WLD lastjoint                        $lastjoint\n";
         if ($lastjoint) {
             $jointstation = (($station+$lastweld)/2);
+#                    print OUT1 "IF WLD jointstation                     $jointstation\n";
             print OUT1 "JNT,$jointstation,$jointtext1,$jointtext2\n";
         }
+	$lastjoint=$text1;
         if ($lastdist) {
             $diststation = (($station+$lastweld)/2);
             print OUT1 "DST,$diststation,$disttext1,\n";
        }
+       $lastdist=$text1;
        print OUT1 "WLD,$station,$text1,\n";
        $lastweld=$station;
     }
     if ($code eq "BND") {
-        $bendtext1=$text1
+        $bendtext1=$text1;
         if ($lastbend) {
             $legstation = (($station+$lastbend)/2);
             print OUT1 "LEG,$legstation,,\n";
-            print OUT1 "BND,$station,$bendtext1,\n;
-            $lastbend=$station;
+            print OUT1 "BND,$station,$bendtext1,\n";
         } else {
             $legstation = (($station+$lastweld)/2);
             print OUT1 "LEG,$legstation,,\n";
-            print OUT1 "BND,$station,$bendtext1,\n;
-            $lastbend=$station;
+            print OUT1 "BND,$station,$bendtext1,\n";
         }
+        $lastbend=$station;
     }
-    if ($code eq "JNT") {
-        $jointtext1=$text1;
-        $jointtext2=$text2;
-    }
+    #-------------------Test Area    
+    # print OUT1 "Status code                                          $code\n";
+    # print OUT1 "Status station                                       $station\n";
+    # print OUT1 "Status text1                                         $text1\n";
+    # print OUT1 "Status text2                                         $text2\n";
+    # print OUT1 "Status bendtext1                                     $bendtext1\n";
+    # print OUT1 "Status diststation                                   $diststation\n";
+    # print OUT1 "Status disttext1                                     $disttext1\n";
+    # print OUT1 "Status jointstation                                  $jointstation\n";
+    # print OUT1 "Status jointtext1                                    $jointtext1\n";
+    # print OUT1 "Status jointtext2                                    $jointtext2\n";
+    # print OUT1 "Status lastbend                                      $lastbend\n";
+    # print OUT1 "Status lastdist                                      $lastdist\n";
+    # print OUT1 "Status lastjoint                                     $lastjoint\n";
+    # print OUT1 "Status lasttext1                                     $lasttext1\n";
+    # print OUT1 "Status lastweld                                      $lastweld\n";
+    # print OUT1 "Status legstation                                    $legstation\n";
 
-
-
-
-##############Test Section
-#print OUT1 "alksdf;alkdj   $pointCodes{$csplit[0]}\n";
-#print OUT5 "\n\n\n\in[0] point number      = $in[0]\n";
-#print OUT5 "in[4] full code & comment      = $in[4]\n";
-#print OUT5 "fsplit[0] full code no comment = $fsplit[0]\n";
-#print OUT5 "fsplit[1] comment              = $fsplit[1]\n";
-#print OUT5 "ssplit[0] code and line no.    = $ssplit[0]\n";
-#print OUT5 "ssplit[1] line code            = $ssplit[1]\n";
-#print OUT5 "tok[0] line code               = $tok[0]\n";
-#print OUT5 "tok[1] code, line no., comment = $tok[1]\n";
-#print OUT5 "csplit[0] alpha code           = $csplit[0]\n";
-#print OUT5 "csplit[1] line number          = $csplit[1]\n";
-#print OUT5 "hold                           = $hold\n";
-#print OUT5 "c linecode                     = $c\n";
-#print OUT5 "Icode idot code, line no.      = $Icode\n\n";
- if  (exists ($bridgeCodes{$csplit[0]}))
-   {
-   if ($c = $idotcommands{$tok[0]})
-     {
-     print OUT2 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$fsplit[1],$c\n";
-     print OUT1 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$fsplit[1],$c\n";
-     }
-   else
-     {
-     print OUT2 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$fsplit[1],\n";
-     print OUT1 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$fsplit[1],\n";
-     }
-
-
-
-
-
-####################### TEST SECTION
-#print OUT5 "in[0] point number             = $in[0]\n";
-#print OUT5 "Icode idot code, line no.      = $Icode\n\n\n\n\n";
-
-
- #prepare for next loop
+#-----------------prepare for next loop
  $jointstation="";
 }
 close(IN);
